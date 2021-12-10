399c3d66cdb3c733e255684e9342712991012f10
Alex Snell <alexksnell@gmail.com>
2021-12-10 18:11:52 -0500

    [REFACTOR] modified pom and driver for SpringBoot web application

---
+++ src/main/java/com/revature/ttapi/TripleTriadDriver.java
@@ -0,0 +1,11 @@
+package com.revature.ttapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleTriadDriver {
+    public static void main(String[] args) {
+        SpringApplication.run(TripleTriadDriver.class, args);
+    }
+}
