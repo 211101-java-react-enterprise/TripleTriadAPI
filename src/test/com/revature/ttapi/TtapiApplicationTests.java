9bc66b08f827723eac66e8c9107ef11f4cc95ba5
Alex Snell <alexksnell@gmail.com>
2021-12-15 02:29:45 -0500

    [Update] Add stub test file

---
+++ src/test/com/revature/ttapi/TtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ttapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TtapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
