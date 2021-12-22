29de75f35775404a77764c6bf9d59618ad65639c
William <Dragonimi1337@gmail.com>
2021-12-21 16:32:47 -0800

    Added and tested Deck and other controller functions and exceptions

--- src/main/java/com/revature/ttapi/deck/dtos/services/DeckRepository.java
+++ src/main/java/com/revature/ttapi/deck/dtos/services/DeckRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Repository
 public interface DeckRepository extends CrudRepository<Deck, UUID> {
     Deck findDeckByDeckNameAndDeckOwner(String name, UUID deckOwner);
-    void deleteDeckById(UUID deckId);
+    ArrayList<Deck> findAllByDeckOwner(UUID deckOwner);
 }
